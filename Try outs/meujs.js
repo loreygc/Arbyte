21771ae274d959d6d7c8f6e59c327bb9466bc545
Lorena <loosgc@gmail.com>
2020-06-18 23:53:12 -0300

    updates

--- Try outs/meujs.js
+++
@@ -1,6 +0,0 @@
-
-let rs= require ('readline-sync')
-let altura=rs.questionFloat("Qual sua altura?");
-let peso=rs.questionFloat("Qual seu peso?");
-let IMC = peso/(altura*altura)
-
--- Try outs/mynewjs.js
+++
@@ -1,3 +0,0 @@
-let rs= require ("readline-sync")
-let salario = rs.question ("qual salario voce gostaria de ter?")
-console.log("Um dia voce tera este salario de" + salario) 
